1ec23dab28efddd239694989f383a8620cf3d9e7
HemantNavlani <hemant.navlani.0506@gmail.com>
2024-06-26 12:16:35 +0530

    Basic set up done Vite Tailwind

---
+++ blog-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    <div className='font-bold flex items-center justify-center mt-40'>
+      Jai Shree Ram | Jai Shree Krishna
+    </div>
+    </>
+  )
+}
+
+export default App
